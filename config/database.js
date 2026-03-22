c606a20bbab22b940ef86904f14f3a277e62ed8d
mailleduc05122004@gmail.com <mailleduc05122004@gmail.com>
2026-03-22 17:49:14 +0700

    Ipda

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
-    process.exit(1);
+    // ❌ bỏ process.exit
   }
 };
 
